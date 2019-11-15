e9f5c30d41c1e690caff464d9c9e0e858800e300
Arturs85 <arturs.ardavs@gmail.com>
2019-11-15 19:44:20 +0200

    Started to make selectable available parts

--- src/GUI/AgentInfo.java
+++ src/GUI/AgentInfo.java
@@ -1,17 +1,23 @@
 package GUI;
 
 import viabots.ManipulatorType;
+import viabots.behaviours.PartType;
+
+import java.util.EnumSet;
+import java.util.Iterator;
+import java.util.Set;
 
 public class AgentInfo {
 
     ManipulatorType type;
     boolean isHardwareReady = false;
     String agentName;
-
+    final Set<PartType> enabledParts; 
     AgentInfo(String name, ManipulatorType type) {
         this.agentName = name;
         this.type = type;
 
+        enabledParts = EnumSet.noneOf(PartType.class);
     }
 
     String getName() {
